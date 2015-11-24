632a99bdd51d650ea9f2164b89455fc9ab9d9449
puzzacertola <giovannimagliozzi@gmail.com>
2015-11-24 16:26:27 +0100

    qualcosa

--- ProgettoPub/src/pub/entita/Ordine.java
+++ ProgettoPub/src/pub/entita/Ordine.java
@@ -2,58 +2,6 @@ package pub.entita;
 
 public class Ordine {
 	
-	//Istanze
-	private int idOrdine;
-	private int idProdotto;
-	private int idTavolo;
-	private int idCameriere;
-	private String Stato;
 	
-	//Costruttore 	
-	public Ordine(){
-		
-	}
-	
-	//Get & Set
-	public int getIdOrdine() {
-		return idOrdine;
-	}	
-	public void setIdOrdine(int idOrdine) {
-		this.idOrdine = idOrdine;
-	}
-	
-	public int getIdProdotto() {
-		return idProdotto;
-	}
-	
-	public void setIdProdotto(int idProdotto) {
-		this.idProdotto = idProdotto;
-	}
-	
-	public int getIdTavolo() {
-		return idTavolo;
-	}
-	
-	public void setIdTavolo(int idTavolo) {
-		this.idTavolo = idTavolo;
-	}
-	
-	public int getIdCameriere() {
-		return idCameriere;
-	}
-	
-	public void setIdCameriere(int idCameriere) {
-		this.idCameriere = idCameriere;
-	}
-	
-	public String getStato() {
-		return Stato;
-	}
-	
-	public void setStato(String stato) {
-		Stato = stato;
-	}
-	
-	//Metodi
-	
+
 }
--- ProgettoPub/src/pub/entita/Snack.java
+++ ProgettoPub/src/pub/entita/Snack.java
@@ -8,3 +8,4 @@ public class Snack extends Prodotti {
 	}
 
 }
+//classe eliminabile?
