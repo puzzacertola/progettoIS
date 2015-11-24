233bf92d4421c9669f0df63ef04600dc230f7673
DoppiaG93 <gillinigiuseppe@gmail.com>
2015-11-24 16:20:55 +0100

    Aggiornato!

---
+++ ProgettoPub/src/pub/entita/Conto.java
@@ -0,0 +1,52 @@
+package pub.entita;
+
+public class Conto {
+	
+	//Istanze
+	private String nomeProdotto;
+	private float costo;
+	private int idTavolo;
+	private float totale;
+	
+	//Costruttore
+	public Conto(){
+		
+	}
+	
+	//Get & Set
+	public String getNomeProdotto() {
+		return nomeProdotto;
+	}
+	
+	public void setNomeProdotto(String nomeProdotto) {
+		this.nomeProdotto = nomeProdotto;
+	}
+	
+	public float getCosto() {
+		return costo;
+	}
+	
+	public void setCosto(float costo) {
+		this.costo = costo;
+	}
+	
+	public int getIdTavolo() {
+		return idTavolo;
+	}
+	
+	public void setIdTavolo(int idTavolo) {
+		this.idTavolo = idTavolo;
+	}
+	
+	public float getTotale() {
+		return totale;
+	}
+	
+	public void setTotale(float totale) {
+		this.totale = totale;
+	}
+	
+	//Metodi
+	
+
+}
---
+++ ProgettoPub/src/pub/entita/Prodotti.java
@@ -0,0 +1,51 @@
+package pub.entita;
+
+public abstract class Prodotti {
+	
+	//Istanze
+	private int idProdotto;
+	private String nome;
+	private String descrizione;
+	private float costo;
+	
+	//Costruttore
+	public Prodotti(){
+		
+	}
+
+	//Get & Set
+	public int getIdProdotto() {
+		return idProdotto;
+	}
+
+	public void setIdProdotto(int idProdotto) {
+		this.idProdotto = idProdotto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public float getCosto() {
+		return costo;
+	}
+
+	public void setCosto(float costo) {
+		this.costo = costo;
+	}
+	
+	//Metodi
+
+}
