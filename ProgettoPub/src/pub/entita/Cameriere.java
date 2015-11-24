1b6b5a11469c11c8cf3fa5f0a8eec04dbf8d3b23
puzzacertola <giovannimagliozzi@gmail.com>
2015-11-24 16:33:55 +0100

    pollo

---
+++ ProgettoPub/src/pub/entita/Cameriere.java
@@ -0,0 +1,44 @@
+package pub.entita;
+
+public class Cameriere {
+	
+	//Istanze
+	private int IdCameriere;
+	private String Nome;
+	private String Cognome;
+	
+	//Costruttore
+	public Cameriere(){
+		
+	}
+
+	//get and setters
+
+	public int getIdCameriere() {
+		return IdCameriere;
+	}
+
+	public void setIdCameriere(int idCameriere) {
+		IdCameriere = idCameriere;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public String getCognome() {
+		return Cognome;
+	}
+
+	public void setCognome(String cognome) {
+		Cognome = cognome;
+	}
+
+	
+	
+	
+}
