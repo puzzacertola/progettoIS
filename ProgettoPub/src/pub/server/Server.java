c074890350e0049cf3a8ed93426c11f993492b7c
DoppiaG93 <gillinigiuseppe@gmail.com>
2015-11-26 11:03:18 +0100

    YEIEAH

--- ProgettoPub/src/pub/server/Server.java
+++ ProgettoPub/src/pub/server/Server.java
@@ -2,42 +2,50 @@
 
 package pub.server;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 
 public class Server {
 	
-	
+	static final int PORTA = 80;
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String USER = "server";
+	static final String PASS = "server";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/IngDelSw";
 	
 	public static void main (String[] args) throws Exception{
 		
 		new Server();
-		try  {
-			String ricevuto=" ";
-			ServerSocket ss= new ServerSocket(80);
-			Socket s=ss.accept();
+		
+		try {
+			String ricevuto = "";
+			ServerSocket ss = new ServerSocket(PORTA);
+			Socket s = ss.accept();
 			
-			PrintWriter out= new PrintWriter (s.getOutputStream(),true);
-			BufferedReader in=new BufferedReader (new InputStreamReader (s.getInputStream()));
+			PrintWriter out = new PrintWriter (s.getOutputStream(),true);
+			BufferedReader in = new BufferedReader (new InputStreamReader(s.getInputStream()));
 			
-			Class.forName("com.mysql.jdbc.Driver");
-			java.sql.Connection conn= DriverManager.getConnection("");//in getConnection ci va il database del tipo "jdbc:Mysql://localhost/amici","cacca","amici"
-			java.sql.Statement stm= conn.createStatement();
-			ricevuto= in.readLine();
-			ResultSet rs= stm.executeQuery(ricevuto);
-			String risposta = new String();
-			out.println("ok\n"+risposta);
-			
-			
+			Class.forName(JDBC_DRIVER);
+			Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			Statement stm = conn.createStatement();
+			ricevuto = in.readLine();
+			ResultSet rs = stm.executeQuery(ricevuto);
+			out.println(rs);
+		
 			ss.close();
 			conn.close();
+		} catch(IOException e){
+			e.printStackTrace();			
 		}
 		
 		
