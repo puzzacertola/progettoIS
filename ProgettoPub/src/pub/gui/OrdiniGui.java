1c39da77e7abd74300016231a4c91a78f50dc470
puzzacertola <giovannimagliozzi@gmail.com>
2015-12-10 16:59:20 +0100

    ordini gui con listener

--- ProgettoPub/src/pub/gui/OrdiniGui.java
+++ ProgettoPub/src/pub/gui/OrdiniGui.java
@@ -3,6 +3,8 @@ package pub.gui;
 import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 import javax.swing.DefaultListModel;
@@ -15,10 +17,11 @@ import javax.swing.JTextArea;
 public class OrdiniGui extends JFrame{
 	private static final long serialVersionUID = 1L;
 	private static JTextArea ordiniTextArea = new JTextArea(20,50);
+	private static JTextArea tavoloTextArea=new JTextArea (20,50);
 
 	private static Container pane ;
 	
-	//creazione interfaccia
+	//creazione interfaccia che mostra gli ordini effettuati ed il loro stato
 	public OrdiniGui(String elenco){
 		
 		super("stato ordini");
@@ -40,12 +43,41 @@ public class OrdiniGui extends JFrame{
 		c.weighty = 1;
 		pane.add(ordiniTextArea, c);
 		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 0;
+		c.gridy = 2;
+		c.weightx = 1;
+		c.weighty = 1;
+		pane.add(new JLabel("tavolo:"), c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 1;
+		c.gridy = 2;
+		c.weightx = 1;
+		c.weighty = 1;
+		pane.add(tavoloTextArea, c);
+		
 		ordiniTextArea.append(elenco);
 		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		pack();
 		setVisible(true);
-				
+		
+		
+		
+		//quando viene inserito il numero di un tavolo filtra gli ordini effettuati per quel tavolo
+		tavoloTextArea.addKeyListener(new KeyListener() {
+			 
+			    public void keyTyped(KeyEvent arg0) {
+			    		//query ordina vista per tavolo
+			    }
+			    @Override
+			    public void keyPressed(KeyEvent arg0) {
+			    }
+			    public void keyReleased(KeyEvent arg0) {
+			    }
+		});
+		
 	}
 	   
 }
